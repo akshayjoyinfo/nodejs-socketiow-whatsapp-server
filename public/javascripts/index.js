097abcec7705b80fca633d4064c7166f560d3786
Akshay Joy <akshayjoyinfo@gmail.com>
2019-12-20 10:51:39 +0530

    Chat rooms integrated and served groups

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,10 +10,11 @@ function CreateChatRooms(){
   var chatRoomName = document.getElementById("chatroomname").value;
   $(".chat-rooms-group").append(`<li class="list-group-item list-group-item-warning">${chatRoomName}</li>`);
   document.getElementById("chatroomname").value = "";
+  console.log(socket);
   socket.emit("chat-rooms-added", {
     channelName: chatRoomName,
     createdAt: new Date(),
-    socketId: socket.socketId,
+    socketId: socket.id,
     channelTextLog:chatRoomName
   });
 }
